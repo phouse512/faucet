81439e8a875a4285e32b4eab8dc86014a971fd5e
phouse512 <philiphouse2015@u.northwestern.edu>
2015-10-30 10:14:10 -0400

    woooot

---
+++ library/api.js
@@ -0,0 +1,12 @@
+//var secrets = require('../secrets.json');
+
+var api = {
+	vars: {
+		baseUrl: 'http://localhost:8000/'
+	},
+	testing: function() {
+		return this.vars.baseUrl;
+	}
+}
+
+exports.api = api;
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var api = require('../library/api');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+  console.log(api.testing());
   res.render('users/list');
 });
 
