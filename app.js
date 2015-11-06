8025dd0a6ca8c571709c4332e4fa2a99ec330481
phouse512 <philiphouse2015@u.northwestern.edu>
2015-11-05 21:45:42 -0600

    adding ui for integrations

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var groups = require('./routes/groups');
+var integrations = require('./routes/integrations');
 
 var app = express();
 
@@ -31,6 +32,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 app.use('/groups', groups);
+app.use('/integrations', integrations);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/integrations.js
+++ routes/integrations.js
@@ -1,1 +1,8 @@
 var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res, next) {
+    res.render('integrations/index', { integrationList: ['github', 'ifttt'] });
+});
+
+module.exports = router;
