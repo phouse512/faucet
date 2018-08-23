56e99628e0e843c5c7bbfd41d1318a59a5e786aa
phouse512 <philiphouse2015@u.northwestern.edu>
2018-08-22 21:26:49 -0500

    resetting project

--- routes/groups.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET users listing. */
-router.get('/:groupId', function(req, res, next) {
-  res.render('groups/group', { groupId: req.params.groupId });
-});
-
-module.exports = router;
--- routes/integrations.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-router.get('/', function(req, res, next) {
-    res.render('integrations/index', { integrationList: ['github', 'ifttt'] });
-});
-
-module.exports = router;
